eaf16b050d9e9791ddde921bc190ab764d217d29
SilverShadow123 <hasanmohasin852@gmail.com>
2025-11-06 22:44:27 +0600

    Refactor: Remove hardcoded API constants
    The hardcoded constants `newsAPIBaseURL`, `newsAPIKey`, `countryQuery`, and `categoryQuery` have been removed from `lib/core/constant/constants.dart`.

--- lib/core/constant/constants.dart
+++
@@ -1,4 +0,0 @@
-const String newsAPIBaseURL = 'https://newsapi.org/v2';
-const String newsAPIKey = '67d80697ffcb4077b51c77f02395d73d';
-const String countryQuery = 'us';
-const String categoryQuery = 'business';
